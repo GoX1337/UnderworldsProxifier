9716c96e7d3cb6ee0e14c7ccd7d12366b93740aa
gox1337 <gox.roche@gmail.com>
2019-12-28 16:45:22 +0100

    Fix after uwdb update

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const request = require('request');
 const cheerio = require('cheerio');
 const download = require('image-downloader');
 
-let baseUrl = "https://www.underworldsdb.com";
-let i = 1;
+const baseUrl = "https://www.underworldsdb.com";
 
 console.log("Underworlds Proxifier...");
 
@@ -20,25 +19,29 @@ request.get(baseUrl, (error, response, body) => {
 const parseResponse = (response) => {
     const $ = cheerio.load(response.body);
     let delay = 0;
-
-    $("#carddb").find('tr').each(function (i, elem) {
-        if ($(this).find('td').eq(0).attr('data-search')) {
-            let card = {
-                id: $(this).find('th').eq(0).text(),
-                name: $(this).find('td').eq(0).attr('data-search'),
-                faction: $(this).find('td').eq(1).attr('data-search').toLowerCase().split(' ').join('_'),
-                type: $(this).find('td').eq(2).attr('data-search').toLowerCase().split(' ').join('_'),
-                expansion: $(this).find('td').eq(6).attr('data-search').toLowerCase().split(' ').join('_'),
-                path: $(this).find('td').eq(0).find('.img-fluid').attr('data-src'),
-            }
-
-            setTimeout(() => {
-                dlImage(card);
-            }, delay);
-
-            delay += 1000;
+    let ii = 0;
+  
+    $("#carddb").find('tbody').find('tr').each(function (i, elem) {
+        ii++;
+        
+        let card = {
+            id: $(this).find('th').eq(0).text(),
+            name: $(this).find('td').eq(0).attr('data-sort'),
+            faction: $(this).find('td').eq(1).attr('data-search').toLowerCase().split(' ').join('_'),
+            type: $(this).find('td').eq(2).attr('data-search').toLowerCase().split(' ').join('_'),
+            expansion: $(this).find('td').eq(6).attr('data-search').toLowerCase().split(' ').join('_'),
+            path: $(this).find('td').eq(0).find('.img-fluid').attr('data-src'),
         }
+
+        console.log(card);
+
+        setTimeout(() => {
+            dlImage(card);
+        }, delay);
+        delay += 1000;
+
     });
+    console.log(ii + " cards");
 }
 
 const createDirectory = (dirName) => {
